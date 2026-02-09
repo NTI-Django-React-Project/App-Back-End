28fd2cee38369d97a06f360735983eab73b6932e
moessam634 <memohamed634@gmail.com>
2026-02-09 02:57:06 +0200

    changing entry point

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -14,8 +14,8 @@ max_retries = 30
 retry_interval = 2
 
 db_config = {
-    "dbname": environ.get("DB_NAME", "postgres"),
-    "user": environ.get("DB_USER", "postgres"),
+    "dbname": environ.get("DB_NAME", "gig-router"),
+    "user": environ.get("DB_USER", "gig_router_user"),
     "password": environ.get("DB_PASSWORD", ""),
     "host": environ.get("DB_HOST", "localhost"),
     "port": environ.get("DB_PORT", "5432"),
